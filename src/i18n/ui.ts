d48d2b85f8c0c44ac4894894d948cf825e5c8486
Waxer59 <78129249+Waxer59@users.noreply.github.com>
2024-10-19 21:57:14 +0200

    Update ui.ts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -18,7 +18,7 @@ export const ui = {
     'meta.description':
       'Desarrollador web con experiencia en liderazgo de equipos ágiles. Participación en proyectos de código abierto y creación de productos web innovadores.',
     'alt.project': 'Proyecto llamado',
-    'aboutMe.description': `<p><strong>¡Soy Hugo!</strong> Desarrollador de software y colaborador de código abierto.</p><p>Disfruto <strong>creando productos de calidad</strong> que ofrezcan excelentes experiencias y sean útiles para los usuarios. Colaboro en varios proyectos de código abierto, lo que me ha permitido <strong>conocer a nuevas personas</strong> y <strong>mejorar mis habilidades blandas</strong>.</p><p>Además, tengo <strong>experiencia liderando equipos</strong> y <strong>formando a compañeros</strong>, ayudándoles a mejorar y desarrollar sus habilidades técnicas. Siempre estoy buscando aprender y mejorar.</p>`,
+    'aboutMe.description': `<p><strong>¡Soy Hugo!</strong> Desarrollador de software y colaborador de código abierto.</p><p>Disfruto <strong>creando productos de calidad</strong> que ofrezcan excelentes experiencias y sean útiles para los usuarios. Colaboro en varios proyectos de código abierto, lo que me ha permitido <strong>conocer a nuevas personas</strong> y <strong>mejorar mis habilidades blandas</strong>.</p><p>Además, tengo <strong>experiencia liderando equipos</strong> y <strong>formando a compañeros</strong>, ayudándoles a mejorar y desarrollar sus habilidades técnicas.</p>`,
     'aboutMe.photo.alt': 'Joven sentado en una roca con vista a la ciudad.',
     'hero.photo.alt': 'Joven con lentes y sudadera azul.',
     'hero.salute': 'Hey, soy Hugo.',
@@ -45,7 +45,7 @@ export const ui = {
     'meta.description':
       'Web developer with experience in agile team leadership. Participation in open source projects and creation of innovative web products.',
     'alt.project': 'Project called',
-    'aboutMe.description': `<p><strong>I'm Hugo!</strong> Software developer and open source contributor.</p><p>I enjoy <strong>creating quality products</strong> that provide excellent experiences and are useful for users. I collaborate on various open source projects, which has allowed me to <strong>meet new people</strong> and <strong>improve my soft skills</strong>.</p><p>Additionally, I have <strong>experience leading teams</strong> and <strong>mentoring colleagues</strong>, helping them improve and develop their technical skills. I am always looking to learn and improve.</p>`,
+    'aboutMe.description': `<p><strong>I'm Hugo!</strong> Software developer and open source contributor.</p><p>I enjoy <strong>creating quality products</strong> that provide excellent experiences and are useful for users. I collaborate on various open source projects, which has allowed me to <strong>meet new people</strong> and <strong>improve my soft skills</strong>.</p><p>Additionally, I have <strong>experience leading teams</strong> and <strong>mentoring colleagues</strong>, helping them improve and develop their technical skills.</p>`,
     'aboutMe.photo.alt': 'Young man sitting on a rock overlooking the city.',
     'hero.photo.alt': 'Young man with glasses and blue sweatshirt.',
     'hero.salute': "Hey, I'm Hugo.",

3228613b53368168af59306262a8f5848a6b2703
waxer59 <hnboyano@gmail.com>
2025-09-09 21:27:12 +0200

    Add experience

---
+++ src/data/experience.ts
@@ -0,0 +1,20 @@
+export const experience = {
+  es: [
+    {
+      company: 'NTT Data',
+      position: 'Practicas Desarrollador Backend',
+      period: 'Marzo 2025 - Junio 2025',
+      description:
+        'Durante mi tiempo en NTT Data, trabajé como Desarrollador Backend utilizando NestJs en un proyecto de gestión de proyectos de ingeniería para CAF. Me encargué de la comunicación con el frontend mediante sockets y servicios REST, dentro de un entorno SAP BTP.'
+    }
+  ],
+  en: [
+    {
+      company: 'NTT Data',
+      position: 'Backend Developer Internship',
+      period: 'March 2025 - June 2025',
+      description:
+        'During my time at NTT Data, I worked as a Backend Developer using NestJs on an engineering project management project for CAF. I was responsible for communication with the frontend using sockets and REST services within an SAP BTP environment.'
+    }
+  ]
+}
